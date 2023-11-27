3bfc70e0bf6f6aadaf2f5bcbc13ea9a4f553e4f7
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-11-27 22:21:31 +0200

    fix task-o

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,4 +1,3 @@
-// Here we will use a const to instantiate a constant var in taskFirst
 export function taskFirst() {
 	const task = 'I prefer const when I can.';
 	return task;
@@ -8,11 +7,9 @@ export function getLast() {
 	return ' is okay';
 }
 
-// Here we will be using let to instantiate a var that may be reassigned in taskNext
 export function taskNext() {
 	let combination = 'But sometimes let';
 	combination += getLast();
 	
 	return combination;
 }
-
