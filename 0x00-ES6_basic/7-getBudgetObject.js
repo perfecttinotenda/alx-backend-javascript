d0bd1c9b026618e21483610449c8103b3a6e5e35
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-11-27 23:02:08 +0200

    Task-7

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,16 @@
+/*
+ * auther: Tinotenda
+ * Use shorthand property names
+ */
+
+export default function getBudgetObject(income, gdp, capita) {
+    // Use shorthand property names
+    const budget = {
+      income,
+      gdp,
+      capita,
+    };
+  
+    return budget;
+  }
+  
