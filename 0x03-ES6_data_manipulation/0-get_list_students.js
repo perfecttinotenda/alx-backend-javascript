a62f42773f4225b862e4deb3e2f8f41480d06623
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-12-05 20:37:17 +0200

    Task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,15 @@
+/*
+    Task: 0-get_list_students.js
+    Author: Tinotenda
+*/
+
+function getListStudents() {
+    return [
+      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+      { id: 2, firstName: 'James', location: 'Columbia' },
+      { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    ];
+  }
+  
+  export default getListStudents;
+  
